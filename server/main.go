f38b7768d11c704247e3b6c60b51d5bf73340114
Mackyson <mackyson72@gmail.com>
2019-09-03 01:11:28 +0900

    雑にいろいろ定義．

--- server/main.go
+++ server/main.go
@@ -2,9 +2,41 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"net/http"
 	"os"
 )
+
+type Player struct {
+	OwnRoom *Room
+	Ws      *websocket.Conn
+	Name    string
+}
+
+func NewPlayer(ownRoom *room, ws *websocket.Conn, name string) {
+	return &Player{
+		OwnRoom: ownRoom,
+		Ws:      ws,
+		Name:    name,
+	}
+}
+
+type Room struct {
+	Players *[2]Player
+	Id      string
+}
+
+func NewRoom(id string) {
+	return &Room{
+		Players: [2]Player{},
+		Id:      id,
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	port := os.Getenv("PORT")
